a1dc68c16a0895a5fa33ddc8504375a0c06ac491
Michael Shea <mjshea3@illinois.edu>
2019-11-07 18:22:14 -0600

    forgot to update index.js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-
-import { NativeModules } from 'react-native';
+import { NativeModules, Image } from 'react-native';
 
 const { RNReactNativeTensorflowlite } = NativeModules;
 
-export default RNReactNativeTensorflowlite;
+class Tflite {
+  loadModel(args, callback) {
+    RNReactNativeTensorflowlite.loadModel(
+      args['modelPath'],
+      args['labelsPath'] || '',
+      (error, response) => {
+        callback && callback(error, response);
+      });
+  }
+
+  runModelOnImage(args, callback) {
+    RNReactNativeTensorflowlite.runModelOnImage(
+      args['path'],
+      args['numResults'] || 5,
+      args['threshold'] != null ? args['threshold'] : 0.1,
+      (error, response) => {
+        callback && callback(error, response);
+      });
+  }
+
+
+  close() {
+    RNReactNativeTensorflowlite.close();
+  }
+}
+
+export default Tflite;
